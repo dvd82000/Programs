1f593fc2b3eceed7d2864119a4c0b76104bc1413
dvd82000 <67478618+dvd82000@users.noreply.github.com>
2021-05-05 00:14:31 -0600

    Fixes #1. Changed operation in the scale method from + to *

--- Circles/src/edu/nmsu/cs/circles/Circle.java
+++ Circles/src/edu/nmsu/cs/circles/Circle.java
@@ -37,7 +37,7 @@ public abstract class Circle
 	 **/
 	public double scale(double factor)
 	{
-		radius = radius * factor;  // Changed operation to *
+		radius = radius * factor;  // Changed operation from + to *
 		return radius;
 	}
 
